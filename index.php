8ab4742a9fbd8fd030092f25dc11e6ef210ec2da
NADIR <nadir.fouka@gmail.com>
2020-08-24 16:58:10 +0200

    add cache component symofny

--- index.php
+++ index.php
@@ -3,5 +3,23 @@
 
 require './vendor/autoload.php' ; 
 
+use Symfony\Contracts\Cache\ItemInterface;
+
+// The callable will only be executed on a cache miss.
+$value = $cache->get('my_cache_key', function (ItemInterface $item) {
+    $item->expiresAfter(3600);
+
+    // ... do some HTTP request or heavy computations
+    $computedValue = 'foobar';
+
+    return $computedValue;
+});
+
+echo $value; // 'foobar'
+
+// ... and to remove the cache key
+$cache->delete('my_cache_key');
+
+
 ?>
 
