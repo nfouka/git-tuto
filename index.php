a6b259abfa6ef91c7e15cd9c1e544e886235691c
nadir.fouka@cea.fr <nadir.fouka@gmail.com>
2020-08-26 14:10:19 +0200

    git

--- index.php
+++ index.php
@@ -1,20 +1,52 @@
 <?php
-require './vendor/autoload.php' ; 
+require_once './vendor/autoload.php';
 
 
-use Symfony\Contracts\Cache\ItemInterface;
 
-// The callable will only be executed on a cache miss.
-$value = $cache->get('my_cache_key', function (ItemInterface $item) {
-    $item->expiresAfter(3600);
-    $computedValue = 'foobar';
+/*---------------------------------------------------------------*/
+/*
+    Titre : Calcul et retourne le temps de chargement des pages
 
-    return $computedValue;
-});
-echo $value; // 'foobar'
-$cache->delete('my_cache_key');
+    URL   : https://phpsources.net/code_s.php?id=166
+    Auteur           : TommyWeb
+    Date édition     : 03 Mars 2006
+    Date mise à jour : 15 Aout 2019
+    Rapport de la maj:
+    - fonctionnement du code vérifié
+*/
+/*---------------------------------------------------------------*/
+    function getmicrotime(){
+        list($usec, $sec) = explode(" ",microtime());
+        return ((float)$usec + (float)$sec);
+    }
+    $debut = getmicrotime();
 
-$build = new PharColletions\FoukaCMS\Builder() ; 
 
-?>
 
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
+// init cache pool of file system adapter
+$cachePool = new FilesystemAdapter('', 0, "cache");
+
+
+
+if ($cachePool->hasItem('demo_string'))
+{
+    $demoString = $cachePool->getItem('demo_string');
+    $variable =  $demoString->get();
+}else{
+    sleep(1) ;
+    $demoString = $cachePool->getItem('demo_string');
+    $demoString->set('Hello World!');
+    $demoString->expiresAfter(5);
+    $cachePool->save($demoString);
+}
+
+
+echo "<h1>hello Cache/Symfony 5  </h1>" ;
+
+
+    $fin = getmicrotime();
+    echo "Page générée en ".round($fin-$debut, 3) ." secondes.<br />";
+
+//$cachePool->clear() ;
