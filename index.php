641a61ae1775fdea93ddc8b53daa70d9848ba6fa
Nadir Fouka <nfouka@users.noreply.github.com>
2020-08-28 22:50:00 +0200

    build project with deployer phar !

--- index.php
+++ index.php
@@ -164,7 +164,7 @@
         <!-- Call to action-->
         <section class="page-section bg-dark text-white">
             <div class="container text-center">
-                <h2 class="mb-4">Free Download at Start Bootstrap!</h2>
+                <h2 class="mb-4">open source solution</h2>
                 <a class="btn btn-light btn-xl" href="https://startbootstrap.com/themes/creative/">Download Now!</a>
             </div>
         </section>
